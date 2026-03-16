ced7aaecd53834b9e4a7e217f604be29258465cf
mahmoudmnael <mahmoudmnael@gmail.com>
2026-03-16 14:14:19 +0200

    feat: add routing for About Us page and remove obsolete .gitinclude file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '/',
+		loadChildren: () =>
+			import('./features/about-us/about-us.routes').then((m) => m.ABOUT_US_ROUTES),
+	},
+];
