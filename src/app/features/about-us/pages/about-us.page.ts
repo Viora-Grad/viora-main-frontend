a5ee2ea3c92571762be7ba15b791449ed1a1be44
mahmoudmnael <mahmoudmnael@gmail.com>
2026-03-26 04:09:38 +0200

    feat: add PrinciplesSection and CtaSection components with templates, styles, and tests to enhance About Us page

--- src/app/features/about-us/pages/about-us.page.ts
+++ src/app/features/about-us/pages/about-us.page.ts
@@ -1,10 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CtaSectionComponent } from '../components/cta-section/cta-section.component';
+import { PrinciplesSectionComponent } from '../components/principles-section/principles-section.component';
 import { PurposeSectionComponent } from '../components/purpose-section/purpose-section.component';
 import { StorySectionComponent } from '../components/story-section/story-section.component';
 
 @Component({
 	selector: 'app-about-us-page',
-	imports: [PurposeSectionComponent, StorySectionComponent],
+	imports: [
+		PurposeSectionComponent,
+		StorySectionComponent,
+		PrinciplesSectionComponent,
+		CtaSectionComponent,
+	],
 	templateUrl: './about-us.page.html',
 	styleUrl: './about-us.page.css',
 	changeDetection: ChangeDetectionStrategy.OnPush,
