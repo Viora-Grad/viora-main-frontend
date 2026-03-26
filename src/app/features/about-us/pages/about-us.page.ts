2bb9ff7b2659f8804cd7b53ae91f092eef56e8c9
mahmoudmnael <mahmoudmnael@gmail.com>
2026-03-26 03:52:22 +0200

    feat: add StorySection component with template, styles, and tests to enhance About Us page

--- src/app/features/about-us/pages/about-us.page.ts
+++ src/app/features/about-us/pages/about-us.page.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { PurposeSectionComponent } from '../components/purpose-section/purpose-section.component';
+import { StorySectionComponent } from '../components/story-section/story-section.component';
 
 @Component({
 	selector: 'app-about-us-page',
-	imports: [PurposeSectionComponent],
+	imports: [PurposeSectionComponent, StorySectionComponent],
 	templateUrl: './about-us.page.html',
 	styleUrl: './about-us.page.css',
 	changeDetection: ChangeDetectionStrategy.OnPush,
