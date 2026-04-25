a8ecfc55133a0f7bbd965ec8c5893ace8ba7e060
Mayam Yehia <yehiamaryam03@gmail.com>
2026-04-25 23:34:40 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
 export const routes: Routes = [
     {path: 'contact', loadChildren: () => import('./features/contact/contact.routes').then(m => m.CONTACT_ROUTES)},
+    
 ];
