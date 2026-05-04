4f80530fda883418b396a6bc0b40ef10d29ebbc1
Mahmoud Nael <mahmoudmnael@gmail.com>
2026-05-04 17:41:39 +0300

    Add route for about us page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ export const routes: Routes = [
 		path: 'pricing',
 		loadChildren: () => import('./features/pricing/pricing.routes').then((m) => m.PRICING_ROUTES),
   },
+	{
 		path: 'about',
 		loadChildren: () => import('./features/about-us/about-us.routes').then((m) => m.ABOUT_US_ROUTES),
   },
