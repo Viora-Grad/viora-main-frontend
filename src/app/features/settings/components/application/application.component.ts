b00b7e2cbf6714838bbc4f54bdc71ffad0a132dd
Mahmoud Nael <mahmoudmnael@gmail.com>
2026-07-07 04:45:59 +0300

    fix(auth): update callback URL to use HTTPS for refresh token

--- src/app/features/settings/components/application/application.component.ts
+++ src/app/features/settings/components/application/application.component.ts
@@ -218,7 +218,7 @@ export class ApplicationComponent implements OnInit {
 		const refreshToken = this._authStore.refreshToken();
 		if (refreshToken) {
 			window.open(
-				`http://${subDomain}.localhost/auth/owner/callback?refreshToken=${encodeURIComponent(refreshToken)}`,
+				`https://${subDomain}.localhost/auth/owner/callback?refreshToken=${encodeURIComponent(refreshToken)}`,
 				'_blank',
 			);
 		}
